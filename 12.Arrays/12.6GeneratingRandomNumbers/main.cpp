b623aa99c3fc6387771c04438458b598a05a54ca
Parasjmail <paras.jmail1234@gmail.com>
2024-04-07 14:01:54 +0530

    new commit

--- 12.Arrays/12.6GeneratingRandomNumbers/main.cpp
+++ 12.Arrays/12.6GeneratingRandomNumbers/main.cpp
@@ -2,7 +2,8 @@
 #include <ctime>
 #include <cstdlib>
 
-int main(){
+int main()
+{
 
     std::srand(std::time(0)); // Seed
 
@@ -14,8 +15,6 @@ int main(){
     random_num = std::rand();
     std::cout << "random_num : " << random_num << std::endl; // 0 ~ RAND_MAX
     */
-   
-
 
     // Generate random numbers in a loop
     /*
@@ -23,10 +22,9 @@ int main(){
 
     for(size_t i {0} ; i < 20 ; ++i){
         random_num = std::rand();
-        std::cout << "random_num " << i << ":" <<  random_num << std::endl; // 0 ~ RAND_MAX       
+        std::cout << "random_num " << i << ":" <<  random_num << std::endl; // 0 ~ RAND_MAX
     }
     */
-    
 
     // Generate a range between 0 and 10
     /*
@@ -34,19 +32,16 @@ int main(){
 
     for(size_t i {0} ; i < 20 ; ++i){
         random_num = std::rand() % 11;
-        std::cout << "random_num " << i << "  :   " <<  random_num << std::endl; // 0 ~ RAND_MAX       
+        std::cout << "random_num " << i << "  :   " <<  random_num << std::endl; // 0 ~ RAND_MAX
     }
     */
-    
 
-
-    int random_num = std::rand() % 10 + 1 ; // [1~10]
-    for(size_t i {0} ; i < 20 ; ++i){
+    int random_num = std::rand() % 10 + 1; // [1~10]
+    for (size_t i{0}; i < 20; ++i)
+    {
         random_num = std::rand() % 10 + 1;
-        std::cout << "random_num " << i << "  :   " <<  random_num << std::endl; // 0 ~ RAND_MAX       
+        std::cout << "random_num " << i << "  :   " << random_num << std::endl; // 0 ~ RAND_MAX
     }
 
- 
-   
     return 0;
 }
